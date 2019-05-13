baf2a8afdbfe8030b0f59cd4cfebe3de982429e0
U-UNIVERSITY\100458008 <100458008@40167EA45B7F.university.ds.derby.ac.uk>
2019-05-13 12:19:31 +0100

    create a basic input script

---
+++ Assets/Scripts/TetrisController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum MoveDirection
+{
+    Left,
+    Right
+}
+
+public enum RotateDirection
+{
+    Clockwise,
+    Anticlockwise
+}
+
+public enum DropType
+{
+    Soft,
+    Hard
+}
+
+public class MoveInput : UnityEvent<MoveDirection> { }
+public class RotateInput : UnityEvent<RotateDirection> { }
+
+public class TetrisController : MonoBehaviour
+{
+    public InputLayout[] inputLayouts;
+    public int selectedInputLayout = 0;
+
+    public UnityEvent<MoveDirection> OnMove { get; private set; }
+    public UnityEvent<RotateDirection> OnRotate { get; private set; }
+    public UnityEvent OnDrop { get; private set; }
+
+    private void Awake()
+    {
+        OnMove = new MoveInput();
+        OnRotate = new RotateInput();
+        OnDrop = new UnityEvent();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        OnMove.AddListener(md => print("move " + md));
+        OnRotate.AddListener(rd => print("rotate" + rd));
+        OnDrop.AddListener(() => print("hard drop"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var layout = inputLayouts[selectedInputLayout];
+
+        // drop
+        // TODO: soft drop
+        if (Input.GetKeyDown(layout.hardDrop))
+        {
+            OnDrop.Invoke();
+            return;
+        }
+
+        // movement
+        if (Input.GetKeyDown(layout.moveLeft))
+        {
+            OnMove.Invoke(MoveDirection.Left);
+            return;
+        }
+        if (Input.GetKeyDown(layout.moveRight))
+        {
+            OnMove.Invoke(MoveDirection.Right);
+            return;
+        }
+
+        // rotation
+        if (Input.GetKeyDown(layout.rotate) || Input.GetKeyDown(layout.rotateAnticlockwise))
+        {
+            OnRotate.Invoke(RotateDirection.Anticlockwise);
+            return;
+        }
+        if (Input.GetKeyDown(layout.rotateClockwise))
+        {
+            OnRotate.Invoke(RotateDirection.Clockwise);
+            return;
+        }
+    }
+}
+
+[Serializable]
+public struct InputLayout
+{
+    // move
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    // rotate
+    public KeyCode rotate;              // defaults to rotate anti-clockwise
+    public KeyCode rotateClockwise;     // anti-clockwise
+    public KeyCode rotateAnticlockwise; // clockwise
+    // drop
+    public KeyCode hardDrop;
+    public KeyCode softDrop;
+
+}
