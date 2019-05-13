7351e2a7df47822d269d327406e2c83a1238a5ea
Joseph Cook <100444037@unimail.derby.ac.uk>
2019-05-13 11:41:56 +0100

    made basic Menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadMain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+}
