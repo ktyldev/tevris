3eae19b7151b9894e77d082ecad2b047d811082f
Joseph Cook <100444037@unimail.derby.ac.uk>
2019-05-13 16:26:13 +0100

    adding temp Gun

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public WeaponScript wP;
+
+    public Rigidbody Bullet;
+
+    public float FireRate = 1f;
+    float Timer = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wP.Firing == true)
+        {
+            Timer -= Time.deltaTime;
+            if (Timer >= 0)
+            {
+                Debug.Log("shooting");
+                Rigidbody Clone;
+                Clone = Instantiate(Bullet, transform.position, transform.rotation);
+
+                Clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+                Timer = FireRate;
+
+            }
+            else
+            {
+                Debug.Log("Reloading");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+    public bool Firing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Firing = true;
+            Debug.Log("Is Firing");
+        }
+	}
+}
