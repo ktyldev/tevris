4f72e2e2874588329ccbd917ec264359dc01692e
cat flynn <me@ktyl.dev>
2019-05-14 13:33:51 +0100

    add borders to board (boarders?)

--- Assets/Scripts/Tetris/TetrisBoard.cs
+++ Assets/Scripts/Tetris/TetrisBoard.cs
@@ -8,6 +8,7 @@ public class TetrisBoard : MonoBehaviour {
 
     public int rows;
     public int columns;
+    public Color borderColour;
 
     private Tetromino[,] tetrominos_;   
     private Piece activePiece_;
@@ -22,7 +23,38 @@ public class TetrisBoard : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
+        CreateBorder();
 	}
+
+    private void CreateBorder()
+    {
+        var borders = new GameObject();
+        borders.transform.SetParent(transform);
+        borders.name = "Borders";
+
+        var botBorder = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        botBorder.transform.position = new Vector3(columns / 2 - 0.5f, -1);
+        botBorder.transform.localScale = new Vector3(columns + 2, 1, 1);
+        botBorder.transform.SetParent(borders.transform);
+
+        var topBorder = Instantiate(botBorder);
+        topBorder.transform.position = new Vector3(columns / 2 - 0.5f, rows);
+        topBorder.transform.SetParent(borders.transform);
+
+        var leftBorder = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        leftBorder.transform.position = new Vector3(-1, rows / 2 - 0.5f);
+        leftBorder.transform.localScale = new Vector3(1, rows, 1);
+        leftBorder.transform.SetParent(borders.transform);
+
+        var rightBorder = Instantiate(leftBorder);
+        rightBorder.transform.position = new Vector3(columns, rows / 2 - 0.5f);
+        rightBorder.transform.SetParent(borders.transform);
+
+        foreach (var renderer in borders.GetComponentsInChildren<Renderer>())
+        {
+            renderer.material.color = borderColour;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
